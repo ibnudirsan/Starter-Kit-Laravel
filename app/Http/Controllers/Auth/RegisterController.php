e4e6675ad89582cde1ef05b8ed062fd7e754087c
rumahdevID <rumahpayment@gmail.com>
2022-10-05 18:43:49 +0700

    init

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,13 +27,6 @@ class RegisterController extends Controller
     use RegistersUsers;
 
     /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::LOGIN;
-
-    /**
      * Create a new controller instance.
      *
      * @return void
