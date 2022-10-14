cfd02b68c5b4fbe240278f68f12ae461192cf1e0
rumahdevID <rumahpayment@gmail.com>
2022-10-14 07:57:02 +0700

    helper

--- app/Models/HasRoles.php
+++ app/Models/HasRoles.php
@@ -6,6 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Role;
 
+/**
+ * App\Models\HasRoles
+ *
+ * @property int $role_id
+ * @property string $model_type
+ * @property int $model_id
+ * @property-read Role|null $roles
+ * @method static \Illuminate\Database\Eloquent\Builder|HasRoles newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|HasRoles newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|HasRoles query()
+ * @method static \Illuminate\Database\Eloquent\Builder|HasRoles whereModelId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HasRoles whereModelType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|HasRoles whereRoleId($value)
+ * @mixin \Eloquent
+ */
 class HasRoles extends Model
 {
     
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -15,6 +15,29 @@ use Spatie\Permission\Traits\RefreshesPermissionCache;
 use Ramsey\Uuid\Uuid as Generator;
 use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
 
+/**
+ * App\Models\Role
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $guard_name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Permission[] $permissions
+ * @property-read int|null $permissions_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $users
+ * @property-read int|null $users_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Role newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role permission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereGuardName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Role extends Model implements RoleContract
 {
 
