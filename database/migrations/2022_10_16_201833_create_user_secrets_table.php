31f3b65aed0265e5c827279bd24a27cf44568db3
rumahdevID <rumahpayment@gmail.com>
2022-10-16 20:24:21 +0700

    user_secrets

---
+++ database/migrations/2022_10_16_201833_create_user_secrets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_secrets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->boolean('isBlock')->default(0);
+            $table->string('secret2Fa')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_secrets');
+    }
+};
