708d7f361bb3f6ea1ecf9e4dc838234857b60600
IDrumahdev <ibnudirsan@gmail.com>
2023-05-30 02:54:54 +0700

    init

--- app/Http/Controllers/Admin/Auth/ProfileController.php
+++ app/Http/Controllers/Admin/Auth/ProfileController.php
@@ -25,38 +25,38 @@ class ProfileController extends Controller
 
     public function ProfileImage(Request $request, $id)
     {
-        // try {            
+        try {            
             $this->ProfileResponse->imageProfile($request, $id);
             $notification = ['message'     => 'Successfully Upload Image Profile.',
                              'alert-type'  => 'success',
                              'gravity'     => 'bottom',
                              'position'    => 'right'];
                 return redirect()->route('profile.index')->with($notification);
-        // } catch (\Exception $e) {
-        //     $notification = ['message'     => 'Failed to Upload Image Profile.',
-        //                      'alert-type'  => 'danger',
-        //                      'gravity'     => 'bottom',
-        //                      'position'    => 'right'];
-        //         return redirect()->route('profile.index')->with($notification);
-        // }
+        } catch (\Exception $e) {
+            $notification = ['message'     => 'Failed to Upload Image Profile.',
+                             'alert-type'  => 'danger',
+                             'gravity'     => 'bottom',
+                             'position'    => 'right'];
+                return redirect()->route('profile.index')->with($notification);
+        }
     }
 
     public function ProfileUpdate(updateProfile $request, $id)
     {
-        // try {
+        try {
             $this->ProfileResponse->updateProfile($request, $id);
             $notification = ['message'     => 'Successfully Updated Data Profile.',
                              'alert-type'  => 'success',
                              'gravity'     => 'bottom',
                              'position'    => 'right'];
                 return redirect()->route('profile.index')->with($notification);
-        // } catch (\Exception $e) {
-        //     $notification = ['message'     => 'Failed to Updated Data Profile.',
-        //                      'alert-type'  => 'danger',
-        //                      'gravity'     => 'bottom',
-        //                      'position'    => 'right'];
-        //         return redirect()->route('profile.index')->with($notification);
-        // }
+        } catch (\Exception $e) {
+            $notification = ['message'     => 'Failed to Updated Data Profile.',
+                             'alert-type'  => 'danger',
+                             'gravity'     => 'bottom',
+                             'position'    => 'right'];
+                return redirect()->route('profile.index')->with($notification);
+        }
     }
 
     public function setting()
