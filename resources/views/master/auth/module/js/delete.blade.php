7b14ab9ad39f1785ee5a4436832ca871033df375
IDrumahdev <ibnudirsan@gmail.com>
2024-09-08 11:37:44 +0700

    Sweet Alert Module

--- resources/views/master/auth/module/js/delete.blade.php
+++ resources/views/master/auth/module/js/delete.blade.php
@@ -1,40 +1,52 @@
-<script type="text/javascript">
-    function isDelete(id) {
-        swal({
-            title: "Confirmation Delete",
-            text: "Are you sure you to Delete Permanent Data Module ?",
-            type: "question",
-            showCancelButton: !0,
-            cancelButtonText: "Batal...",
-            confirmButtonText: "Lanjutkan...",
-        }).then(function (e) {
-            if (e.value === true) {
-                var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+<script>
+    $(document).on('click', '.btn-delete', function (e) {
+        e.preventDefault();
+    
+        var uuid = $(this).data('uuid');
+        Swal.fire({
+            title: 'Confirmation Delete',
+            html: '<p>Are you sure you to Delete Permanent Data Module ?</p>',
+            icon: 'success',
+            showCloseButton: true,
+            showCancelButton: true,
+            focusConfirm: false,
+            confirmButtonText:'<i class="fa fa-thumbs-up"></i> Lanjutkan...',
+            confirmButtonAriaLabel: 'Batal...',
+            cancelButtonText:'<i class="fa fa-thumbs-down"></i> Batal...',
+            cancelButtonAriaLabel:'Lanjutkan...',
+            buttonsStyling: false,
+            customClass: {
+                confirmButton:'btn btn-success mx-2',
+                cancelButton:'btn btn-danger mx-2'
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
                 $.ajax({
-                    type: 'POST',
-                    url: "delete/" + id,
-                    data: {_token: CSRF_TOKEN},
-                    dataType: 'JSON',
-                    success: function (results) {
-                        if (results.success === true) {
-                            swal({
-                                title               : "Successfully",
-                                text                : results.message, 
-                                type                : "success",
-                                showCancelButton    : false,
-                                showConfirmButton   : false,
-                            }),
-                            setInterval(function() { window.location.reload(); }, 1000);
-                        } else {
-                            swal("Failed Process", results.message, "info");
-                        }
+                    url: "delete/" + uuid,
+                    type: 'DELETE',
+                    dataType: 'json',
+                    success: function (data) {
+                        Swal.fire({
+                            title: 'Berhasil',
+                            text: data.message,
+                            icon: 'success',
+                            timer: 2000,
+                            timerProgressBar: true,
+                            showConfirmButton: false
+                        });
+                        setInterval(function() {
+                            window.location.reload();
+                        }, 2000);
+                    },
+                    error: function (xhr, ajaxOptions, thrownError) {
+                        Swal.fire(
+                            'Error!',
+                            'Something went wrong, please try again.',
+                            'error'
+                        );
                     }
                 });
-            } else {
-                e.dismiss;
             }
-        }, function (dismiss) {
-            return false;
-        })
-    }
+        });
+    });
 </script>
--- routes/admin.php
+++ routes/admin.php
@@ -78,7 +78,7 @@ Route::group(['prefix' => '/module'], function () {
             Route::post('/trash/{id}','trash')->name('trash');
             Route::get('/trash','trashData')->name('data.trash');
             Route::post('/restore/{id}','restore')->name('restore');
-            Route::post('/delete/{id}','delete')->name('delete');
+            Route::delete('/delete/{id}','delete')->name('delete');
         });
     });
 });
